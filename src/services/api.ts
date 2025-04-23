a4413f26359a76b0eb2adeaf911a0a729aa71725
MaiaraGilPI <maiaragilfj@gmail.com>
2025-04-23 17:40:17 -0300

    refactor: altera endereço da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Altere para o backend real quando necessário
+  baseURL: 'pji110.mateusrodcosta.dev:3000', // Altere para o backend real quando necessário
 });
 
 // Interceptor para adicionar token JWT automaticamente
